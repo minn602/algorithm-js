1ce381f51cbbd37425b1ba34b5e7d66d43c2d50c
cindy <minn602@gmail.com>
2021-06-24 22:15:47 +0900

    0624 문제풀이

--- 백준/3. for문.js
+++ 백준/3. for문.js
@@ -4,3 +4,12 @@ const n = parseInt(input);
 for (let i = 1; i < 10; i++) {
   console.log(`${n} * ${i} = ${n * i}`);
 }
+
+//a+b
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+for (let i = 1; i <= input[0]; i++) {
+  let numbers = input[i].split(" ");
+
+  console.log(Number(numbers[0]) + Number(numbers[1]));
+}
