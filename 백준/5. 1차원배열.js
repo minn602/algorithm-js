c31c0ab51ab9ae94e9e679675164b31eab63896c
cindy <minn602@gmail.com>
2021-06-28 23:26:31 +0900

    0628 문제풀이

--- 백준/5. 1차원배열.js
+++ 백준/5. 1차원배열.js
@@ -37,4 +37,113 @@ let result = (
   parseInt(input[1]) *
   parseInt(input[2])
 ).toString();
-for (let i = 0; i < result.length; i++) {}
+let arr = new Array(10).fill(0);
+for (let i = 0; i < result.length; i++) {
+  arr[result[i]] += 1;
+}
+for (let x of arr) {
+  console.log(x);
+}
+
+//3052번
+const [...arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+let result = [];
+for (let i = 0; i < arr.length; i++) {
+  result.push(parseInt(arr[i]) % 42);
+}
+
+let count = new Set(result);
+count = [...count].length;
+console.log(count);
+
+//1546번 문제
+//1. 최댓값 구하기 2. 새로운 평균 구하기
+//1. map과 reduce를 이용한 풀이 -> 런타임 에러 -> Math.max를 Number.max로 잘못 적음
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = Number(input[0]);
+let arr = input[1].split(" ");
+
+arr = arr.map((n) => Number(n));
+let max = Math.max(...arr);
+let total = arr.reduce((a, b) => a + (b / max) * 100, 0);
+console.log(total / parseInt(n));
+
+//2. for문을 이용한 풀이
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = Number(input[0]);
+let arr = input[1].split(" ");
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Number(arr[i]);
+}
+
+let max = Math.max(...arr);
+let total = 0;
+for (let i = 0; i < arr.length; i++) {
+  total += (arr[i] / max) * 100;
+}
+console.log(total / parseInt(n));
+
+//8958번
+//1. O가 연속 나오면 스코어에 1씩 더하기 2. X만나면 스코어는 0으로 리셋
+let [n, ...arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+// let num = Number(input[0]);
+
+for (let i = 0; i < arr.length; i++) {
+  let total = 0;
+  let score = 0;
+  for (let j = 0; j < arr[i].length; j++) {
+    if (arr[i][j] === "O") {
+      score += 1;
+    } else {
+      score = 0;
+    }
+    total += score;
+  }
+  console.log(total);
+}
+
+//4344번 문제
+//1. 평균 구하기 2. 평균 점수 넘는 수 구하기 3. 비율 출력
+let [n, ...arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+arr = arr.map((el) => Number(el));
+
+for (let i = 0; i < n; i++) {
+  let cnt = 0;
+  let avg =
+    arr[i].reduce((a, b, i) => {
+      if (i !== 0) {
+        return a + b;
+      }
+    }, 0) / arr[i][0];
+  for (let j = 1; j < arr[0]; j++) {
+    if (arr[i][j] > avg) {
+      cnt++;
+    }
+  }
+  console.log(`${cnt / arr[i][0].toFixed(4)}%`);
+}
